07d564f4e6c39968ca1e70f35066a39ca7273d2e
Ken Ahrens <kahrens@newrelic.com>
2018-01-30 10:57:51 -0500

    adding setup for CopierConfig test

--- src/test/java/com/nrh/api/module/task/metrics/TestCopierConfig.java
+++ src/test/java/com/nrh/api/module/task/metrics/TestCopierConfig.java
@@ -2,14 +2,23 @@ package com.nrh.api.module.task.metrics;
 
 import static org.junit.Assert.*;
 
+import com.nrh.api.APIApplication;
 import com.nrh.api.module.nr.config.MetricConfig;
 import java.util.ArrayList;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 public class TestCopierConfig {
+  
+  @Before
+	public void setUp() throws Exception {
+		
+		// Read in the config files
+    APIApplication.readConfig();
+  }
   
   @Test
   public void testCopierConfig() {
